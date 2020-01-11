d87cb8c221446ab66420f9229907b4c049887885
sjkim <sjkim.pgm@gmail.com>
2020-01-11 19:08:18 +0900

    비정상 종료 시 이전 값을 offset으로 설정

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,16 +6,50 @@ from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
+from django.utils import timezone
+
+
+class Device(models.Model):
+    name = models.CharField(max_length=20)  # same as device_id
+    device_id = models.CharField(max_length=17)
+    device_type = models.CharField(max_length=17, default="default")
+
+    params = JSONField(default=dict)
+
+    latest_datetime = models.DateTimeField(default=timezone.now)
+
+    w1 = models.FloatField(default=0.0)
+    w2 = models.FloatField(default=0.0)
+    D = models.FloatField(default=0.0)
+    dsmall = models.FloatField(default=0.0)
+    H = models.FloatField(default=0.0)
+
+    G_bar = models.FloatField(default=0.0)
+    Q_bar = models.FloatField(default=0.0)
+    F_bar = models.FloatField(default=0.0)
+    theta_x = models.FloatField(default=0.0)
+    theta_y = models.FloatField(default=0.0)
+    theta_z = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return f"{self.name} ({self.device_id}) - {self.device_type}"
 
 
 class Measurement(models.Model):
     device_id = models.CharField(max_length=17)
     datetime = models.DateTimeField()
 
+    # Orignal measurements
     measure_a = models.FloatField()
     measure_b = models.FloatField()
     measure_c = models.FloatField()
     measure_d = models.FloatField()
+
+    # For adv_ver3, 기기가 10분 이상 종료된 경우 이전 값(offset)을 더한 값을 저장.
+    measure_modified_a = models.FloatField(blank=True, default=0)
+    measure_modified_b = models.FloatField(blank=True, default=0)
+    measure_modified_c = models.FloatField(blank=True, default=0)
+    measure_modified_d = models.FloatField(blank=True, default=0)
 
     diff_x = models.FloatField(null=True, blank=True)
     diff_y = models.FloatField(null=True, blank=True)
@@ -31,7 +65,7 @@ class Measurement(models.Model):
             return self.diff_adv_v3(device)
 
     def diff_default(self, device):
-        V = [self.measure_a, self.measure_b, self.measure_c, self.measure_d]
+        V = [self.measure_modified_a, self.measure_modified_b, self.measure_modified_c, self.measure_modified_d]
 
         F_a = device.params['F_a']
         F_b = device.params['F_b']
@@ -52,7 +86,7 @@ class Measurement(models.Model):
         return (x, y, z, a)
 
     def diff_adv_v3(self, device):
-        delta = [self.measure_a, self.measure_b, self.measure_c, self.measure_d]
+        delta = [self.measure_modified_a, self.measure_modified_b, self.measure_modified_c, self.measure_modified_d]
 
         x, y, z, a = np.matmul(device.params['inv_matrix'], delta)
         a = a * 180 / math.pi  # convert rad to deg
@@ -72,97 +106,8 @@ class Measurement(models.Model):
 def measurement_pre_save_callback(sender, **kwargs):
     data = kwargs['instance']
     x, y, z, a = data.diff()
+
     data.diff_x = x
     data.diff_y = y
     data.diff_z = z
     data.diff_a = a
-
-
-class Device(models.Model):
-    name = models.CharField(max_length=20)  # same as device_id
-    device_id = models.CharField(max_length=17)
-    device_type = models.CharField(max_length=17, default="default")
-
-    params = JSONField(default=dict)
-
-    V_A0 = models.FloatField(default=0.0)
-    V_B0 = models.FloatField(default=0.0)
-    V_C0 = models.FloatField(default=0.0)
-    V_D0 = models.FloatField(default=0.0)
-
-    w1 = models.FloatField(default=0.0)
-    w2 = models.FloatField(default=0.0)
-    D = models.FloatField(default=0.0)
-    dsmall = models.FloatField(default=0.0)
-    H = models.FloatField(default=0.0)
-
-    G_bar = models.FloatField(default=0.0)
-    Q_bar = models.FloatField(default=0.0)
-    F_bar = models.FloatField(default=0.0)
-    theta_x = models.FloatField(default=0.0)
-    theta_y = models.FloatField(default=0.0)
-    theta_z = models.FloatField(default=0.0)
-
-    f_Aa = models.FloatField(default=0.0)
-    f_Ab = models.FloatField(default=0.0)
-
-    f_Ba = models.FloatField(default=0.0)
-    f_Bb = models.FloatField(default=0.0)
-
-    f_Ca = models.FloatField(default=0.0)
-    f_Cb = models.FloatField(default=0.0)
-
-    f_Da = models.FloatField(default=0.0)
-    f_Db = models.FloatField(default=0.0)
-
-    L_Ak = models.FloatField(default=0.0)
-    L_Bk = models.FloatField(default=0.0)
-    L_Ck = models.FloatField(default=0.0)
-    L_Dk = models.FloatField(default=0.0)
-
-    M00 = models.FloatField(default=0.0)
-    M01 = models.FloatField(default=0.0)
-    M02 = models.FloatField(default=0.0)
-    M03 = models.FloatField(default=0.0)
-    M10 = models.FloatField(default=0.0)
-    M11 = models.FloatField(default=0.0)
-    M12 = models.FloatField(default=0.0)
-    M13 = models.FloatField(default=0.0)
-    M20 = models.FloatField(default=0.0)
-    M21 = models.FloatField(default=0.0)
-    M22 = models.FloatField(default=0.0)
-    M23 = models.FloatField(default=0.0)
-    M30 = models.FloatField(default=0.0)
-    M31 = models.FloatField(default=0.0)
-    M32 = models.FloatField(default=0.0)
-    M33 = models.FloatField(default=0.0)
-
-    inv00 = models.FloatField(default=0.0)
-    inv01 = models.FloatField(default=0.0)
-    inv02 = models.FloatField(default=0.0)
-    inv03 = models.FloatField(default=0.0)
-    inv10 = models.FloatField(default=0.0)
-    inv11 = models.FloatField(default=0.0)
-    inv12 = models.FloatField(default=0.0)
-    inv13 = models.FloatField(default=0.0)
-    inv20 = models.FloatField(default=0.0)
-    inv21 = models.FloatField(default=0.0)
-    inv22 = models.FloatField(default=0.0)
-    inv23 = models.FloatField(default=0.0)
-    inv30 = models.FloatField(default=0.0)
-    inv31 = models.FloatField(default=0.0)
-    inv32 = models.FloatField(default=0.0)
-    inv33 = models.FloatField(default=0.0)
-
-    x_min = models.FloatField(default=-10.0)
-    y_min = models.FloatField(default=-10.0)
-    z_min = models.FloatField(default=-10.0)
-    t_min = models.FloatField(default=-10.0)
-
-    x_max = models.FloatField(default=10.0)
-    y_max = models.FloatField(default=10.0)
-    z_max = models.FloatField(default=10.0)
-    t_max = models.FloatField(default=10.0)
-
-    def __str__(self):
-        return f"{self.name} ({self.device_id}) - {self.device_type}"
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from .models import Measurement, Device
 
@@ -15,6 +16,41 @@ class MeasurementSerializer(serializers.ModelSerializer):
         model = Measurement
         fields = ('device_id', 'datetime', 'measure_a', 'measure_b',
                   'measure_c', 'measure_d', 'diff_x', 'diff_y', 'diff_z', 'diff_a')
+
+    def create(self, validated_data):
+        obj = Measurement(**validated_data)
+        device = get_object_or_404(Device, device_id=obj.device_id)
+
+        # For default, not apply offset
+        if device.device_type == 'default':
+            obj.measure_modified_a = obj.measure_a
+            obj.measure_modified_b = obj.measure_b
+            obj.measure_modified_c = obj.measure_c
+            obj.measure_modified_d = obj.measure_d
+        else:
+            # Check whether offset should be updated
+            curr = obj.datetime
+            prev = device.latest_datetime
+
+            if (curr - prev).seconds > 10 * 60:
+                prev = Measurement.objects.filter(device_id=obj.device_id).latest('datetime')
+                device.params['offset'] = [prev.measure_modified_a,
+                                           prev.measure_modified_b,
+                                           prev.measure_modified_c,
+                                           prev.measure_modified_d]
+
+            # Update device's latest_datetime
+            device.latest_datetime = obj.datetime
+            device.save()
+
+            offset = device.params.get('offset', [0, 0, 0, 0])
+            obj.measure_modified_a = obj.measure_a + offset[0]
+            obj.measure_modified_b = obj.measure_b + offset[1]
+            obj.measure_modified_c = obj.measure_c + offset[2]
+            obj.measure_modified_d = obj.measure_d + offset[3]
+
+        obj.save()
+        return obj
 
 
 class DeviceSerializer(serializers.ModelSerializer):
